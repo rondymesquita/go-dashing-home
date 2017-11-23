ba2e7aad02b54c5e501bddd737918e076b4fcb99
Rondinelli Mesquita <rondinellimesquitas@gmail.com>
2017-11-23 16:06:05 -0300

    remove unused widgets

--- sample/www/widgets/app/app.js
+++ sample/www/widgets/app/app.js
@@ -33,22 +33,22 @@ Vue.component('app-widget', {
     },
     methods:{
         open: function(){
-            // request = $.ajax({
-            //     url: "/command/exec",
-            //     type: "POST",
-            //     data: "{\"Commands\": [[\"echo\", \"hello\"], [\"firefox\"]]}",
-            //     dataType: "json"
-            // });
-            //
-            // request.done(function (data) {
-            //     console.log(data)
-            //     self.set('button', 'Fulano');
-            // });
-            //
-            // request.fail(function (data) {
-            //     console.log("fail")
-            //     console.log(data)
-            // });
+            request = $.ajax({
+                url: "/command/exec",
+                type: "POST",
+                data: "{\"Commands\": [[\"echo\", \"hello\"], [\"firefox\"]]}",
+                dataType: "json"
+            });
+
+            request.done(function (data) {
+                console.log(data)
+                self.set('button', 'Fulano');
+            });
+
+            request.fail(function (data) {
+                console.log("fail")
+                console.log(data)
+            });
         }
     }
 });
